8b15aa18c16e878ec9c912bdbc9ea137157aa0c4
Damien Bell <damiensbell@gmail.com>
2017-03-28 16:20:45 -0500

    saving

--- server.js
+++ server.js
@@ -21,17 +21,10 @@ app.get("/dreams", function (request, response) {
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
+app.get("/trump", function (request, response) {
+    response.sendFile(__dirname + '/views/index.html');
 });
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
