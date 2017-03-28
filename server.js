e1a5c7f220fcd07a1fd65908d58ff5e0644b9c67
Damien Bell <damiensbell@gmail.com>
2017-03-28 16:43:56 -0500

    opps, my bad

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get("/test", function (request, response) {
+  response.sendFile(__dirname + '/views/test.html');
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
